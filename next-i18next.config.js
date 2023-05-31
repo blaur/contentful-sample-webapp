466d0e268045c77e520a78fe33fc211959055bec
Brian Bak Laursen <brianbaklaursen@gmail.com>
2023-05-31 12:26:57 +0200

    removing locales for now

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 module.exports = {
   i18n: {
     defaultLocale: 'en-US',
     locales: ['en-US', 'de-DE'],
     localeDetection: false,
-    localePath: path.resolve('./public/locales'),
+    // localePath: path.resolve('./public/locales'),
   },
 };
